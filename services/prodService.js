4346ac10b7f8ed7114d2645b4cef6a71968c5e12
Victor Varges <victorryandias@gmail.com>
2022-02-04 00:02:08 -0300

    trying to validate test

--- services/prodService.js
+++ services/prodService.js
@@ -2,23 +2,26 @@ const productsModel = require('../models/productsModel');
 
 const productsNameValidate = async (name) => {
   const listProductsDb = await productsModel.getAllProducts();
-  const nameAlreadyExists = listProductsDb.some((product) => product.name === name); 
-  
-  if (!name) return { code: 400, message: '"name" is required' }; 
+  console.log();
+  console.log({ listProductsDb });
+  const nameAlreadyExists = listProductsDb.some((product) => product.name === name);
+
+  if (!name) return { code: 400, message: '"name" is required' };
 
   if (name.length < 5) {
- return { 
-    code: 422, message: '"name" length must be at least 5 characters long' }; 
-} 
+    return {
+      code: 422, message: '"name" length must be at least 5 characters long',
+    };
+  }
 
-  if (nameAlreadyExists) return { code: 409, message: 'Product already exists' }; 
+  if (nameAlreadyExists) return { code: 409, message: 'Product already exists' };
 };
 
 const productsQuantityValidate = (quantity) => {
   if (quantity === undefined) {
     return {
-        code: 400,
-        message: '"quantity" is required',
+      code: 400,
+      message: '"quantity" is required',
     };
   }
   if (typeof quantity !== 'number' || quantity < 1) {
@@ -39,39 +42,40 @@ const productsValidate = async (name, quantity) => {
     return invokeName;
   }
   const insertInDB = await productsModel.createProducts({ name, quantity });
-    return insertInDB;
+  return insertInDB;
 };
 
 const getProductsValidate = async () => {
   const productsInDb = await productsModel.getAllProducts();
 
-  return productsInDb; 
+  return productsInDb;
 };
 
 const getProductsIdValidate = async (id) => {
   const productsInDb = await productsModel.getProductsId(id);
   if (productsInDb === null) return { code: 404, message: 'Product not found' };
 
-  return productsInDb; 
+  return productsInDb;
 };
 
 const updateValidate = async ({ name, quantity, id }) => {
   const productsInDb = await productsModel.getProductsId(id);
 
   if (!productsInDb) return { code: 404, message: 'Product not found' };
-  
-  if (name.length < 5) {
-    return { 
-       code: 422, message: '"name" length must be at least 5 characters long' }; 
-   } 
 
-   if (typeof quantity !== 'number' || quantity < 1) {
-     return {
+  if (name.length < 5) {
+    return {
+      code: 422, message: '"name" length must be at least 5 characters long',
+    };
+  }
+
+  if (typeof quantity !== 'number' || quantity < 1) {
+    return {
       code: 422,
       message: '"quantity" must be a number larger than or equal to 1',
     };
   }
-      const updateInDb = await productsModel.updateProducts({ name, quantity, id });
+  const updateInDb = await productsModel.updateProducts({ name, quantity, id });
 
   return updateInDb;
 };
@@ -86,6 +90,7 @@ const deleteValidate = async (id) => {
 };
 
 module.exports = {
+  productsNameValidate,
   productsValidate,
   getProductsValidate,
   getProductsIdValidate,
